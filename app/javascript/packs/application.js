3b19446bdf495bbcb5012638749fc9825f45f79b
David <friday10292001@yahoo.com.tw>
2023-07-03 15:17:04 +0800

    設定首頁及使用bootstrap

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,6 +3,9 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
+require("script")
+require("stylesheets")
+
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
