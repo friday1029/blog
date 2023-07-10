d929810215502fb409a8dac06e0a2577b1252b86
David <friday10292001@yahoo.com.tw>
2023-07-10 21:29:26 +0800

    後台架構

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  root 'pages#index'
   resources :posts, only: [:index, :show]
   resources :works, only: [:index, :show]
   get 'about', to: 'pages#about'
-  root 'pages#index'
+  namespace :admins, path: 'yellow_mouse_3847' do 
+    get '/', to: 'pages#index'
+  end
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
