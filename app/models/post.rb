8e6fbc61bdabc7628de60e19d975b8a5f2840d57
David <friday10292001@yahoo.com.tw>
2023-07-16 17:34:11 +0800

    加上 carrierwave 及 annotate 套件

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,14 @@
+# == Schema Information
+#
+# Table name: posts
+#
+#  id         :bigint           not null, primary key
+#  content_en :text
+#  content_zh :text
+#  title_en   :string
+#  title_zh   :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class Post < ApplicationRecord
 end
--- app/models/work.rb
+++ app/models/work.rb
@@ -1,2 +1,15 @@
+# == Schema Information
+#
+# Table name: works
+#
+#  id         :bigint           not null, primary key
+#  desc_en    :text
+#  desc_zh    :text
+#  name_en    :string
+#  name_zh    :string
+#  site_url   :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class Work < ApplicationRecord
 end
