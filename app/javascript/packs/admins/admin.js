293ffefa38e874b3975f3611c1523bc9f880e9bd
David <friday10292001@yahoo.com.tw>
2023-07-17 17:43:30 +0800

    不使用 turbolinks

--- app/javascript/packs/admins/admin.js
+++ app/javascript/packs/admins/admin.js
@@ -7,11 +7,9 @@ require("script/admins")
 require("stylesheets/admins/admin.scss")
 
 import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import "../common/quill-editor/quill-editor"
 
 Rails.start()
-Turbolinks.start()
 ActiveStorage.start()
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,10 +7,8 @@ require("script")
 require("stylesheets")
 
 import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
 Rails.start()
-Turbolinks.start()
 ActiveStorage.start()
