5af1316282765894bd00f0e7867ad01fa6edea9a
David <friday10292001@yahoo.com.tw>
2023-08-03 15:59:16 +0800

    fix: The related packages used by Babel have been renamed.

--- babel.config.js
+++ babel.config.js
@@ -53,14 +53,14 @@ module.exports = function(api) {
           useBuiltIns: true
         }
       ],
-      [
-        '@babel/plugin-proposal-private-methods',
+      [ 
+        '@babel/plugin-transform-private-methods',
         {
           loose: true
         }
       ],
       [
-        '@babel/plugin-proposal-private-property-in-object',
+        '@babel/plugin-transform-private-property-in-object',
         {
           loose: true
         }
